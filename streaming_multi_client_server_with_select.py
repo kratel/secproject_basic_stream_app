64f3d302e07f130da548fe55b462a5ef42870d9c
kratel <santi.f.salas@gmail.com>
2021-04-28 22:50:48 -0400

    Added select features to handle multiple clients and work with non-blocking sockets instead of threading.

--- streaming_multi_client_server_with_select.py
+++ streaming_multi_client_server_with_select.py
@@ -17,6 +17,7 @@ import pickle
 import struct
 # Needed to handle non-blocking server socket
 import select
+import queue
 
 # Globals for handling the frames
 outputFrame = None
@@ -24,6 +25,7 @@ lock = threading.Lock()
 stream = True
 read_list = []
 write_list = []
+message_queues = {}
 
 
 # thread that listens for any input, used to terminate stream loop
@@ -34,7 +36,7 @@ def key_capture_thread(server_socket):
     print("starting exit process")
 
 def capture_frames():
-    global outputFrame, lock, stream
+    global outputFrame, lock, stream, message_queues
     # threading.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
     while stream:
         # Grab a screenshot
@@ -52,6 +54,10 @@ def capture_frames():
 
         with lock:
             outputFrame = frame.copy()
+            for sq in message_queues:
+                serializedFrame = pickle.dumps(outputFrame)
+                message = struct.pack("Q",len(serializedFrame))+serializedFrame
+                message_queues[sq].put(message)
             # cv2.imshow("RECEIVING VIDEO",outputFrame)
             # cv2.waitKey()
 
@@ -67,10 +73,16 @@ def new_client(client_socket):
         try:
             while stream:
                 # img,frame = vid.read()
-                with lock:
-                    serializedFrame = pickle.dumps(outputFrame)
-                    message = struct.pack("Q",len(serializedFrame))+serializedFrame
-                    client_socket.sendall(message)
+                readable, writable, errored = select.select(read_list, write_list, [], 20)
+                for s in writable:
+                    if s is client_socket:
+                        with lock:
+                            serializedFrame = pickle.dumps(outputFrame)
+                            message = struct.pack("Q",len(serializedFrame))+serializedFrame
+                            s.sendall(message)
+                # for s in readable:
+                #     if s is client_socket:
+                #         s.recv(1024)
                     
                 if outputFrame is not None:
                     pass
@@ -118,7 +130,11 @@ if __name__ == '__main__':
     write_list = []
     try: 
         while stream:
-            readable, writable, errored = select.select(read_list, write_list, [], 20)
+            print("Getting ready to select")
+            readable, writable, errored = select.select(read_list, write_list, read_list, 20)
+            print(readable)
+            print(writable)
+            print(errored)
             for s in readable:
                 if s is server_socket:
                     print("waiting for connection - before accept")
@@ -126,13 +142,59 @@ if __name__ == '__main__':
                     if client_socket:
                         print("waiting for connection - after accept")
                         print('GOT CONNECTION FROM: %s:%s' % (caddr, cport))
-                        connThread = threading.Thread(target=new_client, args=(client_socket,))
-                        threads.append(connThread)
+                        client_socket.setblocking(False)
                         read_list.append(client_socket)
+                        message_queues[client_socket] = queue.Queue()
+                        # with lock:
+                        #     serializedFrame = pickle.dumps(outputFrame)
+                        #     message = struct.pack("Q",len(serializedFrame))+serializedFrame
+                        #     message_queues[s].put(message)
                         write_list.append(client_socket)
-                        connThread.start()
+                else:
+                    print("reading from a non server socket")
+                    data = s.recv(1024)
+                    print("reading from a non server socket")
+                    if data == b'READY':
+                        # with lock:
+                        #     serializedFrame = pickle.dumps(outputFrame)
+                        #     message = struct.pack("Q",len(serializedFrame))+serializedFrame
+                        #     message_queues[s].put(message)
+                        if s not in write_list:
+                            write_list.append(s)
+                    else:
+                        print("going to remove a non server socket")
+                        if s in write_list:
+                            write_list.remove(s)
+                        read_list.remove(s)
+                        s.close()
+                        del message_queues[s]
+                        # connThread = threading.Thread(target=new_client, args=(client_socket,))
+                        # threads.append(connThread)
+                        # read_list.append(client_socket)
+                        # write_list.append(client_socket)
+                        # connThread.start()
+            for s in writable:
+                print("in write_list")
+                try:
+                    print("in try in write_list")
+                    next_msg = message_queues[s].get_nowait()
+                except queue.Empty:
+                    print("in except in write_list")
+                    # write_list.remove(s)
+                else:
+                    print("sending in write_list")
+                    s.sendall(next_msg)
+
+            for s in errored:
+                print("in errored")
+                read_list.remove(s)
+                if s in writable:
+                    write_list.remove(s)
+                s.close()
+                del message_queues[s]
+
             if not stream:
-                read_list.pop()
+                # read_list.pop()
                 break
             # if client_socket:
             #     # vid = cv2.VideoCapture(0)
