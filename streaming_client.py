324fb268617651b77563f0429401c1ac64592651
kratel <santi.f.salas@gmail.com>
2021-05-01 04:34:46 -0400

    Working diffie helman key exchange.

--- streaming_client.py
+++ streaming_client.py
@@ -5,6 +5,13 @@ import struct
 import threading
 import argparse
 import errno
+# For encryption
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import PublicFormat, Encoding, load_der_public_key
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 watching = True
 
@@ -15,6 +22,67 @@ def key_capture_thread():
     watching = False
     print("starting exit process")
 
+def encrypt(key, plaintext, iv):
+    # Declare cipher type
+    cipher = Cipher(algorithms.AES(key), modes.OFB(iv))
+    encryptor = cipher.encryptor()
+
+    # Encrypt
+    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+
+    return ciphertext
+
+def decrypt(key, ciphertext, iv):
+    # Declare cipher type
+    cipher = Cipher(algorithms.AES(key), modes.OFB(iv))
+    decryptor = cipher.decryptor()
+
+    # Decrypt
+    deciphered_text = decryptor.update(ciphertext) + decryptor.finalize()
+
+    return deciphered_text
+
+def generate_dh_key_pairs():
+    # Hard-coded p and g for DH Key exchange (RFC 3526 - group id 14)
+    p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
+    g = 2
+
+    # Use our p and g with cryptography library
+    params_numbers = dh.DHParameterNumbers(p,g)
+    parameters = params_numbers.parameters(default_backend())
+
+    # Generate private and public key
+    host_private_key = parameters.generate_private_key()
+    host_public_key_enc= host_private_key.public_key().public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
+    return (host_private_key, host_public_key_enc)
+
+def client_dh_key_exchange(host_socket, host_private_key, host_public_key_enc):
+    # Send Message to let server know it's going to send the public key
+    # host_socket.sendall(b"PUBK")
+    # Send size of public key and public key to remote
+    host_socket.send(len(host_public_key_enc).to_bytes(2, "big") + host_public_key_enc)
+    print("Sent host's public key to", host_ip, ":", port)
+    
+    # Receiving size of remote's public key and remote's public key
+    size = host_socket.recv(2)
+    remote_public_key_enc = host_socket.recv(int.from_bytes(size, "big"))
+    print("Size of remote's public key: ", int.from_bytes(size, "big"))
+    print("Remote's public key:\n", remote_public_key_enc)
+
+    # Decode remote's public key
+    remote_public_key = load_der_public_key(remote_public_key_enc, default_backend())
+
+    # Generate shared key
+    shared_key = host_private_key.exchange(remote_public_key)
+    print("Shared Key:\n", shared_key)
+
+    return shared_key
+
+def receive_and_decrypt_AES_OFB_message(host_socket, derived_key, derived_iv):
+    size = host_socket.recv(2)
+    ciphertext = host_socket.recv(int.from_bytes(size, "big"))
+    deciphered_text = decrypt(derived_key, ciphertext, derived_iv)
+    return deciphered_text
 
 if __name__ == '__main__':
     # Handle arguments
@@ -32,8 +100,22 @@ if __name__ == '__main__':
 
     threading.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
     try:
+        # Generate new dh key pairs before each connection
+        client_private_key, client_public_key_enc = generate_dh_key_pairs()
         # Initialize Connection
         client_socket.connect((host_ip,port)) # a tuple
+
+        client_socket.sendall(b"HELO")
+        # === DH KEY EXCHANGE START ===
+
+        shared_key = client_dh_key_exchange(client_socket, client_private_key, client_public_key_enc)
+
+        # Derive Key from shared key, length is in byte (32 byte = 256 bit)
+        derived_key = HKDF(algorithm=hashes.SHA256(),length=32,salt=None,info=b'handshake data',).derive(shared_key)
+        print("Derived Key:\n", derived_key)
+
+        # === DH KEY EXCHANGE END ===
+
         # initialize data var
         data = b""
         # Specify size as 8 bytes
--- streaming_multi_client_server_with_select.py
+++ streaming_multi_client_server_with_select.py
@@ -18,6 +18,13 @@ import struct
 # Needed to handle non-blocking server socket
 import select
 import queue
+# For encryption
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.serialization import PublicFormat, Encoding, load_der_public_key
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 # Globals for handling the frames
 outputFrame = None
@@ -26,6 +33,8 @@ stream = True
 read_list = []
 write_list = []
 message_queues = {}
+dh_keyexchanges = {}
+client_derived_keys_ivs = {}
 
 
 # thread that listens for any input, used to terminate stream loop
@@ -62,41 +71,97 @@ def capture_frames():
             # cv2.waitKey()
 
         # time.sleep(0.5)
-        print("captured a screenshot")
-        print(stream)
+        # print("captured a screenshot")
+        # print(stream)
 
-def new_client(client_socket):
-    global lock, stream, outputFrame, read_list, write_list
-    if client_socket:
-        # vid = cv2.VideoCapture(0)
-        # global outputFrame, lock
-        try:
-            while stream:
-                # img,frame = vid.read()
-                readable, writable, errored = select.select(read_list, write_list, [], 20)
-                for s in writable:
-                    if s is client_socket:
-                        with lock:
-                            serializedFrame = pickle.dumps(outputFrame)
-                            message = struct.pack("Q",len(serializedFrame))+serializedFrame
-                            s.sendall(message)
-                # for s in readable:
-                #     if s is client_socket:
-                #         s.recv(1024)
+def encrypt(key, plaintext, iv):
+    # Declare cipher type
+    cipher = Cipher(algorithms.AES(key), modes.OFB(iv))
+    encryptor = cipher.encryptor()
+
+    # Encrypt
+    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+
+    return ciphertext
+
+def decrypt(key, ciphertext, iv):
+    # Declare cipher type
+    cipher = Cipher(algorithms.AES(key), modes.OFB(iv))
+    decryptor = cipher.decryptor()
+
+    # Decrypt
+    deciphered_text = decryptor.update(ciphertext) + decryptor.finalize()
+
+    return deciphered_text
+
+def generate_dh_key_pairs():
+    # Hard-coded p and g for DH Key exchange (RFC 3526 - group id 14)
+    p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA18217C32905E462E36CE3BE39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9DE2BCBF6955817183995497CEA956AE515D2261898FA051015728E5A8AACAA68FFFFFFFFFFFFFFFF
+    g = 2
+
+    # Use our p and g with cryptography library
+    params_numbers = dh.DHParameterNumbers(p,g)
+    parameters = params_numbers.parameters(default_backend())
+
+    # Generate private and public key
+    host_private_key = parameters.generate_private_key()
+    host_public_key_enc= host_private_key.public_key().public_bytes(Encoding.DER, PublicFormat.SubjectPublicKeyInfo)
+    return (host_private_key, host_public_key_enc)
+
+def server_dh_key_exchange(client_socket, host_private_key, host_public_key_enc):
+    # Send size of public key and public key to remote
+    client_socket.send(len(host_public_key_enc).to_bytes(2, "big") + host_public_key_enc)
+    print("Sent host's public key to ", caddr, ":", cport)
+
+    # Receiving size of remote's public key and remote's public key
+    size = client_socket.recv(2)
+    remote_public_key_enc = client_socket.recv(int.from_bytes(size, "big"))
+    print("Size of remote's public key: ", int.from_bytes(size, "big"))
+    print("Remote's public key:\n", remote_public_key_enc)
+
+    # Decode remote's public key
+    remote_public_key = load_der_public_key(remote_public_key_enc, default_backend())
+
+    # Generate shared key
+    shared_key = host_private_key.exchange(remote_public_key)
+    return shared_key
+
+def encrypt_and_send_AES_OFB_message(client_socket, plaintext, key, iv):
+    ciphertext = encrypt(key, plaintext, iv)
+    client_socket.send(len(ciphertext).to_bytes(2, "big") + ciphertext)
+
+# def new_client(client_socket):
+#     global lock, stream, outputFrame, read_list, write_list
+#     if client_socket:
+#         # vid = cv2.VideoCapture(0)
+#         # global outputFrame, lock
+#         try:
+#             while stream:
+#                 # img,frame = vid.read()
+#                 readable, writable, errored = select.select(read_list, write_list, [], 20)
+#                 for s in writable:
+#                     if s is client_socket:
+#                         with lock:
+#                             serializedFrame = pickle.dumps(outputFrame)
+#                             message = struct.pack("Q",len(serializedFrame))+serializedFrame
+#                             s.sendall(message)
+#                 # for s in readable:
+#                 #     if s is client_socket:
+#                 #         s.recv(1024)
                     
-                if outputFrame is not None:
-                    pass
-                    # # Show the image, debugging
-                    # cv2.imshow('SERVER STREAMING VIDEO',outputFrame)
-                    # # Way to close the feed, required for imshow to work properly
-                    # key = cv2.waitKey(1) & 0xFF
-                    # if key ==ord('q') or not stream:
-                    #     # client_socket.close()
-                    #     break
-        finally:
-            client_socket.close()
-            write_list.remove(client_socket)
-            read_list.remove(client_socket)
+#                 if outputFrame is not None:
+#                     pass
+#                     # # Show the image, debugging
+#                     # cv2.imshow('SERVER STREAMING VIDEO',outputFrame)
+#                     # # Way to close the feed, required for imshow to work properly
+#                     # key = cv2.waitKey(1) & 0xFF
+#                     # if key ==ord('q') or not stream:
+#                     #     # client_socket.close()
+#                     #     break
+#         finally:
+#             client_socket.close()
+#             write_list.remove(client_socket)
+#             read_list.remove(client_socket)
 
 
 if __name__ == '__main__':
@@ -145,51 +210,85 @@ if __name__ == '__main__':
                         client_socket.setblocking(False)
                         read_list.append(client_socket)
                         message_queues[client_socket] = queue.Queue()
+                        dh_keyexchanges[client_socket] = generate_dh_key_pairs()
                         # with lock:
                         #     serializedFrame = pickle.dumps(outputFrame)
                         #     message = struct.pack("Q",len(serializedFrame))+serializedFrame
                         #     message_queues[s].put(message)
                         # write_list.append(client_socket)
                 else:
-                    print("reading from a non server socket")
-                    data = s.recv(1024)
-                    print("reading from a non server socket")
-                    if data == b'READY':
-                        with lock:
-                            serializedFrame = pickle.dumps(outputFrame)
-                            message = struct.pack("Q",len(serializedFrame))+serializedFrame
-                            message_queues[s].put(message)
-                        if s not in write_list:
-                            write_list.append(s)
-                    elif data == b'LEAVING':
-                        if s in write_list:
-                            write_list.remove(s)
-                        read_list.remove(s)
-                        s.close()
-                        del message_queues[s]
+                    if s in dh_keyexchanges:
+                        print("in dh key exchange")
+                        data = s.recv(4)
+                        print(data)
+                        if data == b'HELO':
+                            message_queues[s].put(len(dh_keyexchanges[s][1]).to_bytes(2, "big") + dh_keyexchanges[s][1])
+                            if s not in write_list:
+                                write_list.append(s)
+                        # elif data == b'PUBK':
+                            print(readable)
+                            print(writable)
+                            print(errored)
+                            # Receiving size of remote's public key and remote's public key
+                            size = s.recv(2)
+                            remote_public_key_enc = s.recv(int.from_bytes(size, "big"))
+                            print("Size of remote's public key: ", int.from_bytes(size, "big"))
+                            print("Remote's public key:\n", remote_public_key_enc)
+
+                            # Decode remote's public key
+                            remote_public_key = load_der_public_key(remote_public_key_enc, default_backend())
+
+                            # Generate shared key
+                            shared_key = dh_keyexchanges[s][0].exchange(remote_public_key)
+                            derived_key = HKDF(algorithm=hashes.SHA256(),length=32,salt=None,info=b'handshake data',).derive(shared_key)
+                            print("Derived Key:\n", derived_key)
+                            derived_iv = HKDF(algorithm=hashes.SHA256(),length=16,salt=None,info=b'aes ofb iv',).derive(shared_key)
+                            print("Derived IV:\n", derived_iv)
+                            client_derived_keys_ivs[s] = (derived_key, derived_iv)
+                            del dh_keyexchanges[s]
+                            # if s not in write_list:
+                            #     write_list.append(s)
                     else:
-                        print("going to remove a non server socket")
-                        if s in write_list:
-                            write_list.remove(s)
-                        read_list.remove(s)
-                        s.close()
-                        del message_queues[s]
-                        # connThread = threading.Thread(target=new_client, args=(client_socket,))
-                        # threads.append(connThread)
-                        # read_list.append(client_socket)
-                        # write_list.append(client_socket)
-                        # connThread.start()
+                        print("reading from a non server socket")
+                        data = s.recv(1024)
+                        print("reading from a non server socket")
+                        if data == b'READY':
+                            with lock:
+                                serializedFrame = pickle.dumps(outputFrame)
+                                message = struct.pack("Q",len(serializedFrame))+serializedFrame
+                                message_queues[s].put(message)
+                            if s not in write_list:
+                                write_list.append(s)
+                        elif data == b'LEAVING':
+                            if s in write_list:
+                                write_list.remove(s)
+                            read_list.remove(s)
+                            s.close()
+                            del message_queues[s]
+                        else:
+                            print("going to remove a non server socket")
+                            if s in write_list:
+                                write_list.remove(s)
+                            read_list.remove(s)
+                            s.close()
+                            del message_queues[s]
+                            # connThread = threading.Thread(target=new_client, args=(client_socket,))
+                            # threads.append(connThread)
+                            # read_list.append(client_socket)
+                            # write_list.append(client_socket)
+                            # connThread.start()
             for s in writable:
                 print("in write_list")
                 try:
                     print("in try in write_list")
                     next_msg = message_queues[s].get_nowait()
                 except queue.Empty:
+                    # if not s in dh_keyexchanges:
                     print("in except in write_list")
                     write_list.remove(s)
                 else:
                     print("sending in write_list")
-                    s.sendall(next_msg)
+                    s.send(next_msg)
 
             for s in errored:
                 print("in errored")
